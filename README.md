e97acb1bf8aab1bc8d57ef143a4ced2091205964
leo-PeD-Hion <leonardo.tancredi@hiontecnologia.com.br>
2023-06-22 17:03:37 -0300

    v0.2 reset+analize requisicoes

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ RECEBER DADOS DE IOT VIA TRANSMISSÃO LORA
 ## v0.1_payload_incremento
 * inicialização do sistema lendo o payload recebido e decriptografando da base64 
 * faz incremento a cada post 
+
+
+## v0.2 reset+analize requisicoes
+* requisição de reset requisição  HTTP `GET`  via `/reset`
+* agora em `/lorahion` cada requisição HTTP é contada e, também, as requisições bem sucedidas. Dessa forma é apresentado a contagem de cada uma e a porcetagem de dado efetivamente perdido. 
+* TAMBÉM estou lendo a contagem que o lora passa no body `req.body.params.counter_up`
--- index.js
+++ index.js
@@ -19,10 +19,22 @@
 
 // sistemas para ler a requisição http que virá via json
     const bodyParser = require('body-parser');
+const { reset } = require('nodemon');
     app.use(bodyParser.json());
 
 // constante global para incrementar a cada requisição
     app.vini = 0;
+    app.httpRecebido = 0;
+    let resultado = 0;
+//rota para recetar as variáveis
+            app.get('/reset', function (req, res) {
+                                app.vini = 0;
+                                app.httpRecebido = 0;
+                                resultado = 0;
+                                console.log("resetar as variáveis internas");
+                                return res.status(200).send("Reiniciado as variáveis");
+
+                                }); 
 
 // rota de acesso via http . Nela está a função de tratamento dos dados recebidos 
     app.post('/lorahion', function (req, res) {
@@ -30,19 +42,33 @@
         //quando recebeu a requisição
             let timer = new Date();
             timer = timer.toLocaleString('pt-BR',{timeZone:'America/Sao_Paulo'});
-            console.log('=====NOVO NOVO ===============================');
-            console.log('Seu pacote chegou com sucesso!');
-            console.log(timer);
-            console.log('Contagem: ',app.vini)
-            console.log('  ');
-
-        //contador de requisições recebidas 
-            app.vini ++;
+            app.httpRecebido ++;    
+        
 
         if((req.body.params.payload != undefined || req.body.params.payload != null)){
             //dado da requisição
-                console.log("req.body");
-                console.log(req.body);
+                console.log("CONTAGEM DO LORA ");
+                console.log("req.body.params.counter_up");
+                console.log(req.body.params.counter_up);
+            //contador de requisições recebidas 
+                app.vini ++;
+            //printas valores 
+            console.log('=====NOVO NOVO ===============================');
+            console.log('Seu pacote chegou com sucesso!');
+            console.log(timer);
+            console.log('  ');
+            console.log('Contagem que efetivamente eu recebi: ',app.vini);
+            console.log('  ');
+            console.log('Contagem http request: ',app.httpRecebido);
+            console.log('  ');
+            resultado = app.httpRecebido - app.vini;
+            console.log('Diferença entre request recebido vazio para recebido com payload: ',resultado);
+            console.log('  ');
+            console.log('  ');
+            resultado = Math.floor(100*( app.httpRecebido - app.vini)/app.httpRecebido);
+            console.log('Porcentagem de mensagens vazias: ',resultado);
+
+    
                    
             //decriptografar a base 64
                 const msg_decrypt = Buffer.from(req.body.params.payload, 'base64').toString('utf-8')
@@ -50,6 +76,7 @@
                 let bytes = [];
                 console.log('O pacote decriptografado é:');
                 console.log(msg_decrypt);
+                console.log('  ');
         }
 
         return res.status(200).send("AQUI VINI! 200"); //resposta a requisição http
